2f2fc53361a829712bacb892d0466e3e3a5411fa
maians01 <maiana.sanjuan@phs.scot>
2024-09-30 18:36:43 +0100

    feat: tidyhealthboard shares and indices trend data for app

--- app/set-up.R
+++ app/set-up.R
@@ -8,8 +8,50 @@
 #' Memory:16GB
 #' CPUs:1
 
+#start_time = timestamp()
 # load packages ----
+library(here)
+library(glue)
+library(openxlsx)
+library(magrittr)
+library(janitor)
 library(shiny)
 
+
 # load functions ----
-source("functions/core-functions.R")
+source(here("app/functions/core-functions.R"))
+
+# parameters ----
+
+import_data <- ifelse(length(list.files(here("data-pack"))) == 0, TRUE, FALSE)
+
+tidy_data <- ifelse(length((list.files(here("app/data/")))) == 0, TRUE, FALSE)
+
+data_years <- jsonlite::fromJSON("lookups/data-urls.json") %>%
+  select(target_year_start, target_year_end)
+
+# create dirs ----
+if(!("data" %in% list.dirs(here("app"), full.names = FALSE))){
+  dir.create(here("app", "data"))
+}
+
+# import and clean data ----
+
+# data takes approx 20 min to download
+if(isTRUE(import_data)){
+   
+  for(i in 1:nrow(data_years)){
+    
+    data_years_row <- data_years %>% slice(i)
+    
+    source(here("import-app-data.R"), 
+           local = list2env(list(start_year = data_years_row$target_year_start,
+                            end_year = data_years_row$target_year_end)))
+    
+  }
+}
+
+if(isTRUE(tidy_data)){
+  source(here("tidy-app-data.R"), local = TRUE)
+}
+#end_time = timestamp()
