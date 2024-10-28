72759d264fc0c690fdb8b552f1bcb826693d497a
maiana-sanjuan <maiana.sanjuan@psh.scot>
2024-10-28 12:48:40 +0100

    fix!: update the folder structure and set-up to prevent bugs with app deployment

--- app/pages/populations/populations-ui.R
+++ app/pages/populations/populations-ui.R
@@ -1,1 +1,6 @@
-tagList(titlePanel("Populations"))
+tagList(titlePanel("Populations"),
+        layout_sidebar("Population by Healthboard", 
+                       selectInput("hb_in_pop", "Chose a Healthboard:", choices = hb_list)),
+        layout_sidebar("Healthboard Comparison", "plot2"),
+        layout_sidebar("Population History", "plot3")
+        ) #taglist
