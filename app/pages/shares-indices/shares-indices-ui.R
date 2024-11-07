e119a55ecc0c4d7a0c3d591112550c95ba6b4f8f
maiana-sanjuan <maiana.sanjuan@psh.scot>
2024-11-07 11:50:22 +0100

    feat: age-sex shares and indices plot based on user filters

--- app/pages/shares-indices/shares-indices-ui.R
+++ app/pages/shares-indices/shares-indices-ui.R
@@ -1,29 +1,30 @@
-tagList(titlePanel("Shares and Indices"),
-        layout_sidebar(
-          title = "Population by Healthboard", 
-          sidebar = sidebar(
-            radioButtons("stat_in_shares", "Chose a statistic:", 
-                         choices = stat_list),
-            radioButtons("programme_in_shares", "Chose a Care Programme:", 
-                         choices = programme_list)
-          ),
-          accordion(
-            open = c("Time Series by Component", 
-                     "Percentage Change by Component"), 
-            accordion_panel("Time Series by Component", 
-                      navset_card_underline(
-                        nav_panel("Plot",
-                                  highchartOutput("test_plot")
-                                  ),
-                        nav_panel("Table")
-                      )
-            ),
-            accordion_panel("Percentage Change by Component", 
-                      navset_card_underline(
-                        nav_panel("Plot"),
-                        nav_panel("Table")
-                      )
-            )
-        ) #accordion
-      ) #sidebar
+tagList(
+  titlePanel("Shares and Indices"),
+  layout_sidebar(
+    title = "Population by Healthboard",
+    sidebar = sidebar(
+      radioButtons(
+        "stat_in_shares",
+        "Chose a statistic:",
+        choices = as.character(shares_indices_list$stat)
+      ),
+      radioButtons(
+        "programme_in_shares",
+        "Chose a Care Programme:",
+        choices = as.character(shares_indices_list$care_programme)
+      )
+    ),
+    
+    accordion(
+      open = c("Time Series by Component", "Percentage Change by Component"),
+      accordion_panel(
+        "Time Series by Component",
+        navset_card_underline(nav_panel("Plot", highchartOutput("test_plot")), nav_panel("Table"))
+      ),
+      accordion_panel(
+        "Percentage Change by Component",
+        navset_card_underline(nav_panel("Plot"), nav_panel("Table"))
+      )
+    ) #accordion
+  ) #sidebar
 ) #taglist
